937c4e1009969e4f7bd2e0a4eaa184dc495f6c6c
Àlex Magaz Graça <alexandre.magaz@gmail.com>
2025-01-05 12:51:31 +0100

    alexm: Set up build flavors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -65,6 +65,18 @@ android {
         }
     }
 
+    flavorDimensions += "version"
+    productFlavors {
+        create("devel") {
+            resValue("string", "app_name", "S2 (Dev)")
+        }
+        create("alexm") {
+            applicationIdSuffix = ".alexm"
+            versionNameSuffix = "-alexm"
+            resValue("string", "app_name", "S2 (alexm)")
+        }
+    }
+
     packaging {
         resources {
             resources.excludes += setOf("META-INF/*.kotlin_module")
@@ -88,6 +100,8 @@ android {
 
     buildFeatures {
         compose = true
+        buildConfig = true
+        resValues = true
     }
 
     namespace = "com.simplecityapps.shuttle"
