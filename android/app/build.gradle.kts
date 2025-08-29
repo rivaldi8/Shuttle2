2f4733a48fdf4c57a54bddfeae6004f14f14fe4b
Àlex Magaz Graça <alexandre.magaz@gmail.com>
2025-08-29 10:23:46 +0200

    Add song thumbnails

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -247,6 +247,7 @@ android {
             // Excludes the support library because it's already included by Glide.
             isTransitive = false
         }
+        implementation(libs.glide.compose)
 
         // About Libraries
         implementation(libs.mikepenz.aboutlibrariesCore)
